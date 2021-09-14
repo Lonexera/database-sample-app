6cdd493fa5ba0038a98852c8ecd26b3a3bfe7100
tania <tatianatolkachyova@gmail.com>
2021-09-15 00:43:56 +0300

    feat: add DogsApplication class

---
+++ app/src/main/java/com/databasesampleapp/DogsApplication.kt
@@ -0,0 +1,29 @@
+package com.databasesampleapp
+
+import android.app.Application
+import com.databasesampleapp.db.cursor.CursorDatabaseOpenHelper
+import com.databasesampleapp.db.cursor.CursorRepository
+import com.databasesampleapp.db.room.DogRoomDatabase
+import com.databasesampleapp.db.room.RoomRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class DogsApplication : Application() {
+
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
+    private val roomDatabase: DogRoomDatabase by lazy {
+        DogRoomDatabase.getDatabase(
+            this,
+            applicationScope
+        )
+    }
+
+    private val roomRepository by lazy { RoomRepository(roomDatabase.dogDao()) }
+
+    private val cursorDatabase: CursorDatabaseOpenHelper by lazy {
+        CursorDatabaseOpenHelper(this)
+    }
+
+    private val cursorRepository by lazy { CursorRepository(cursorDatabase) }
+}
